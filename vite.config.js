14aedeaf61376ff937d7cbd3845a5e1476724869
Jose Rojas <joserojas.cartes@gmail.com>
2024-09-24 16:26:59 -0300

    Hito7

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"https://JoseRojasC.github.io/Hito-PizzeriaMammaMia-7"
+  base:"https://joserojasc.github.io/Hito-PizzeriaMammaMia-7/"
 })
