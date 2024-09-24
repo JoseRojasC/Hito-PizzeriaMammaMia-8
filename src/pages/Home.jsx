d1a16162de75bb9d3652643ab90412792e354786
Jose Rojas <joserojas.cartes@gmail.com>
2024-09-24 17:41:43 -0300

    Hito 7 v3

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,25 @@
 import React, { useEffect, useState } from 'react';
 import CardPizza from '../components/CardPizza';
-import { useCartContext } from '../Context/CartContext'; 
+import { useCartContext } from '../Context/CartContext';
+import { pizzas as localPizzas } from '../data/pizzas'; 
 
 const Home = () => {
   const [pizzas, setPizzas] = useState([]);
-  const { addToCart } = useCartContext(); 
+  const { addToCart } = useCartContext();
 
   const getPizzas = async () => {
-    const respuesta = await fetch('http://localhost:5000/api/pizzas'); 
-    const pizzas = await respuesta.json();
-    setPizzas(pizzas);
+    try {
+      const respuesta = await fetch('http://localhost:5000/api/pizzas');
+      if (!respuesta.ok) {
+        throw new Error('Error al obtener las pizzas del backend');
+      }
+      const pizzas = await respuesta.json();
+      setPizzas(pizzas);
+    } catch (error) {
+      console.error('Fallo la llamada a la API, usando pizzas locales:', error);
+      setPizzas(localPizzas);
+    }
   };
-  
 
   useEffect(() => {
     getPizzas();
@@ -23,8 +31,8 @@ const Home = () => {
         {pizzas.map(pizza => (
           <CardPizza
             key={pizza.id}
-            pizza={pizza} 
-            addToCart={() => addToCart(pizza)} 
+            pizza={pizza}
+            addToCart={() => addToCart(pizza)}
           />
         ))}
       </div>
@@ -33,4 +41,3 @@ const Home = () => {
 };
 
 export default Home;
-
